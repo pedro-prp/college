2f965bfef56e5df8be2fcfe1df659817b2e8ec6d
Pedro Rodrigues <peddroprp@gmail.com>
2018-04-10 11:48:50 -0300

    implementação de leitura de cabecalho correta

--- src/imagem.cpp
+++ src/imagem.cpp
@@ -1,5 +1,6 @@
 #include "imagem.hpp"
 #include <iostream>
+#include <string>
 #include <fstream>
 
 using namespace std;
@@ -37,20 +38,47 @@ void Imagem::abrirArquivo(){
 }
 
 
-
 void Imagem::lerArquivo(){
-	char copia;
+
+	string inicioCrip,tamCrip,cifra,tipo;
+	char comentario,copia[10000000000000000000];
+	int i=1,j=1;
+
+
+	getline(arquivoFile,tipo,'\n');
+	arquivoFile.get(comentario);
+	getline(arquivoFile,inicioCrip,' ');
+	getline(arquivoFile,tamCrip,' ');
+	getline(arquivoFile,cifra,'\n');
+
 	ofstream arquivoSaida;
 	arquivoSaida.open("./imagens/jose.pgm");
 
-	int cont=1;
-	
+	ofstream arquivoDaCrip;
+	arquivoDaCrip.open("./txt/crip.txt");
+
+	arquivoSaida << tipo << endl;
+	arquivoSaida << comentario << inicioCrip << " " << tamCrip << " " << cifra << endl;
+
 	while(!arquivoFile.eof()){
 		
-			arquivoFile.get(copia);
-		cout << copia;
-		arquivoSaida << copia;
-		cont++;
+		arquivoFile.get(copia[i]);
+
+		cout << (int)copia[i];
+		arquivoSaida << (int)copia[i];
+		i++;
+
+		if(i==(int)inicioCrip){
+			arquivoDaCrip << copia[i];
+			for(i=inicioCripInt;j<=(incioCripInt+tamCripInt);i++){
+				arquivoFile.get(copia[i]);
+
+				arquivoSaida << copia[i];
+				arquivoDaCrip << copia[i];
+			}
+		}
 	}
 	cout << endl;
+
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,11 @@ int main(int argc, char ** argv){
 
 	Imagem jose;
 
-	string arquivo;
-	cin >> arquivo;
+	string arquivo = "exemplo.pgm";
+	//cin >> arquivo;
 
 	jose.setArquivo(arquivo);
 	jose.abrirArquivo();
 	jose.lerArquivo();
-
-
 
 }
