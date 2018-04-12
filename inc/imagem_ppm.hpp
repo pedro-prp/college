0d0f36e2b3231b16cf45dd277245fb156e673a04
Pedro Rodrigues <peddroprp@gmail.com>
2018-04-12 09:28:44 -0300

    inicio do desenvolvimento de descriptografia em ppm

---
+++ inc/imagem_ppm.hpp
@@ -0,0 +1,18 @@
+#ifndef IMAGEM_PPM_HPP
+#define IMAGEM_PPM_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include <ctype.h>
+
+
+class ImagemPPM {
+	
+	private:
+
+	public:
+
+};
---
+++ src/imagem_ppm.cpp
@@ -0,0 +1,7 @@
+#include "imagem_ppm.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include <ctype.h>
