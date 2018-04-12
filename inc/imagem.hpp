4b1e9f1bce5482a20ceda2d8aa91df7acbe58d23
Pedro Rodrigues <peddroprp@gmail.com>
2018-04-12 10:24:43 -0300

    correção de falha manual

---
+++ inc/imagem.hpp
@@ -0,0 +1,39 @@
+#ifndef IMAGEM_HPP
+#define IMAGEM_HPP
+
+#include "imagem.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+
+class Imagem {
+
+private:
+	string arquivo;
+	string camArquivo;
+	ifstream arquivoFile;
+
+public:
+
+	Imagem();
+	~Imagem();
+
+
+	void setArquivo(string arquivo);
+	string getArquivo();
+	
+	string getCamArquivo();
+
+	void abrirArquivo();
+
+	void lerArquivo();
+
+	void descriptografar();
+};
+
+#endif
--- src/imagem_ppm.cpp
+++
@@ -1,7 +0,0 @@
-#include "imagem_ppm.hpp"
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <stdlib.h>
-#include <vector>
-#include <ctype.h>
