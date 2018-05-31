a0bc85c8c85cbc5b4447ad80a323501290aa92e8
Pedro Rodrigues <peddroprp@gmail.com>
2018-05-31 14:21:28 -0300

    checkBarcoExplodiu funcionando horizontal e verticalmente

--- src/JogoBatalhaNaval/Check/NavioPart.java
+++ src/JogoBatalhaNaval/Check/NavioPart.java
@@ -70,8 +70,34 @@ public class NavioPart {
 			if((direitaN + esquerdaN + 1) == MatrixCampo.getMatrixInt(j,i)) {
 				return true;
 			}
+		}else if(orientacao == "vertical") {
 			
-			
+			cimaN = 0;
+			baixoN = 0;
+			int a = 1;
+			//direita
+			while(a < MatrixCampo.getMatrixInt(j, i)) {
+				//baixo
+				if(baixo) {
+					if((j+a) < 14) {
+						if(MatrixCampo.getMatrixInt(j, i) == MatrixCampo.getMatrixSemiExplodeInt((j+a),i)) {
+							baixoN++;
+						}
+					}
+				}
+				//cima
+				if(cima) {
+					if((j-a ) > 0) {
+						if(MatrixCampo.getMatrixInt(j, i) == MatrixCampo.getMatrixSemiExplodeInt((j-a),i)) {
+							cimaN++;
+						}
+					}
+				}
+				a++;
+			}
+			if((baixoN + cimaN + 1) == MatrixCampo.getMatrixInt(j,i)) {
+				return true;
+			}
 		}
 		
 		return false;
