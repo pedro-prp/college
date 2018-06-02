d51e7c1107cc40024ef152ecbdb188472ce14977
Pedro Rodrigues <peddroprp@gmail.com>
2018-06-02 15:03:02 -0300

    tela de finalização para caso acabe a mana

--- src/JogoBatalhaNaval/states/FinalState.java
+++ src/JogoBatalhaNaval/states/FinalState.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import JogoBatalhaNaval.Player.Mana;
+import JogoBatalhaNaval.grafico.Assets;
 
 public class FinalState extends State{
 
@@ -16,11 +17,11 @@ public class FinalState extends State{
 	@Override
 	public void desenha(Graphics grafico) {
 		if(Mana.getMana() == 0) {
-			grafico.setColor(Color.BLUE);
+			grafico.drawImage(Assets.semMana,0,0,null);
 		}else {
 			grafico.setColor(Color.BLACK);
+			grafico.fillRect(0, 0, 955,754);
 		}
-		grafico.fillRect(0, 0, 955,754);
 		
 	}
 
