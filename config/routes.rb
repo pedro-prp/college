8cc9af16d7dec3b15be6ef9c27bcf6a69946b245
pedro_prp <peddroprp@gmail.com>
2018-07-02 03:01:02 -0300

    the creation of a review is synchonized with movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
   devise_for :users
-  resources :movies
+  resources :movies do
+  	 resources :reviews, except: [:index,:show]
+  end
 
   root to: "movies#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
