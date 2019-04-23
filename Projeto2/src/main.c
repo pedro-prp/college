9398b17c9d977e4f5bd59305a22b5b74ac88a80c
pedro-prp <peddroprp@gmail.com>
2019-04-23 20:21:16 -0300

    Refactors code and adapts to dynamic allocation

--- Projeto2/src/main.c
+++ Projeto2/src/main.c
@@ -4,93 +4,129 @@
 
 void avanc_string(char *v);
 
+double get_area_pixel(int i, int j, int *matrix, int *area);
+void set_binary_matrix(int *area,int *binario, double sum);
+void transform_bin_caracol(char *bin_str,int *binario);
+int get_ilbp_numb(char *bin_str);
+void set_ilbp_number_vector(int *number_ilbp,int result_ilbp);
+
 int main(){
 
-    // ler imagem
     FILE *f;
 
     f = fopen("DataSet/asphalt/asphalt_01.txt", "r");
 
-    int matrix[1026][1026];
+    int *matrix = (int*) malloc(1026*1026*sizeof(int));
 
     for(int i = 0 ;i < 1025; i++){
         for(int j = 0; j < 1025; j++){
             char c;
-            fscanf(f,"%d%c",&matrix[i][j],&c);
+            fscanf(f,"%d%c", (matrix+(i*1026)+j),&c);
         }
     }
+
+    int *number_ilbp = (int*) calloc(512,sizeof(int));
 
     // pega um pixel
-    double sum = 0;
-    for(int i = 1; i < 2; i++){
-        for(int j = 1; j < 5; j++){
-            sum = 0;
-            // int *area;
-            // area = (int*) malloc(3*3*sizeof(int));
-            int area[3][3];
-            int binario[3][3];
-
-            int lin = 0, col = 0;
-            for(int a = i-1; a < i+2; a++){
-                col = 0;
-                for(int b = j-1; b < j+2;b++){
-                    area[lin][col] = matrix[a][b];
-                    printf("%d ",area[lin][col]);
-                    sum+= area[lin][col];
-                    col++;
-                }
-                lin++;
-                printf("\n");
-            }
-            printf("%lf\n",sum/9);
-            // faz a matriz binaria
-            for(int a = 0; a < 3;a++){
-                for(int b = 0; b < 3; b++){
-                    // if(*(area+(a*3)+b) > (sum/9))
-                    if(area[a][b] > (sum/9))
-                        binario[a][b] = 1;
-                    else
-                        binario[a][b] = 0;
-
-                    printf("%d %lf %d\n",area[a][b],(sum/9),binario[a][b]);
-                }
-            }
+    for(int i = 1; i < 1024; i++){
+        for(int j = 1; j < 1024; j++){
+            // Pega area envolta dos pixels
+            int *area = (int*) malloc(3*3*sizeof(int));
+            double sum = get_area_pixel(i, j, matrix, area);
+            
+            // Seta a matriz binaria
+            int *binario = (int*) malloc(3*3*sizeof(int));
+            set_binary_matrix(area, binario, sum);
 
             // caracol
+            char *bin_str = (char*) malloc(9*sizeof(char));
+            transform_bin_caracol(bin_str, binario);
 
-            char bin_str[9];
+            // Pega o resultado do ilbp
+            int result_ilbp = get_ilbp_numb(bin_str);
 
-            bin_str[0] = (char)binario[0][0]+48;
-            bin_str[1] = (char)binario[0][1]+48;
-            bin_str[2] = (char)binario[0][2]+48;
-            bin_str[3] = (char)binario[1][2]+48;
-            bin_str[4] = (char)binario[2][2]+48;
-            bin_str[5] = (char)binario[2][1]+48;
-            bin_str[6] = (char)binario[2][0]+48;
-            bin_str[7] = (char)binario[1][0]+48;
-            bin_str[8] = (char)binario[1][1]+48;
+            // Seta vetor ilbp resultados
+            set_ilbp_number_vector(number_ilbp,result_ilbp);
 
-            int result_ilbp = (int) strtol(bin_str, NULL, 2);
-
-            for(int a = 0; a < 8;a++){
-                avanc_string(&bin_str);
-                
-                if((int) strtol(bin_str, NULL, 2) < result_ilbp)
-                    result_ilbp = (int) strtol(bin_str, NULL, 2);
-                
-            }
-
-            printf("%d\n",result_ilbp);
-
-            for(int a = 0; a < 3; a++)
-                for(int b = 0; b < 3; b++)
-                    area[a][b] = 0;
+            // limpa memoria
+            free(area);
+            free(bin_str);
+            free(binario);
         }
     }
 
+    // Print vetor ilbp
+    for(int i = 0; i < 512; i++)
+        if(*(number_ilbp + i) != 0)
+            printf("%d = %d\n",i,*(number_ilbp+i));
+    printf("\n");
+
+    // Limpa memoria
+    free(number_ilbp);
+    free(matrix);
+    
+    // Fecha arquivo
+    fclose(f);
     
     return 0;
 }
+
+double get_area_pixel(int i, int j,int *matrix, int *area){
+    int lin = 0, col = 0;
+    double sum = 0;
+    for(int a = i-1; a < i+2; a++){
+        col = 0;
+        for(int b = j-1; b < j+2;b++){
+            *(area+(lin*3)+col) = *(matrix+(a*1026)+b);
+            sum+= *(area+(lin*3)+col);
+            col++;
+        }
+        lin++;
+    }
+
+    return sum;
+}
+
+void set_binary_matrix(int *area,int *binario, double sum){
+    for(int a = 0; a < 3;a++){
+        for(int b = 0; b < 3; b++){
+            if(*(area+(a*3)+b) > (sum/9))
+                *(binario+(a*3)+b) = 1;
+            else
+                *(binario+(a*3)+b) = 0;
+        }
+    }
+}
+
+void transform_bin_caracol(char *bin_str,int *binario){
+    *(bin_str + 0) = (char)(*(binario+(3*0)+0))+48;
+    *(bin_str + 1) = (char)(*(binario+(3*0)+1))+48;
+    *(bin_str + 2) = (char)(*(binario+(3*0)+2))+48;
+    *(bin_str + 3) = (char)(*(binario+(3*1)+2))+48;
+    *(bin_str + 4) = (char)(*(binario+(3*2)+2))+48;
+    *(bin_str + 5) = (char)(*(binario+(3*2)+1))+48;
+    *(bin_str + 6) = (char)(*(binario+(3*2)+0))+48;
+    *(bin_str + 7) = (char)(*(binario+(3*1)+0))+48;
+    *(bin_str + 8) = (char)(*(binario+(3*1)+1))+48;
+}
+
+int get_ilbp_numb(char *bin_str){
+    int result_ilbp = (int) strtol(bin_str, NULL, 2);
+    for(int a = 0; a < 8;a++){
+        avanc_string(bin_str);
+        
+        if((int) strtol(bin_str, NULL, 2) < result_ilbp)
+            result_ilbp = (int) strtol(bin_str, NULL, 2);
+        
+    }
+    return result_ilbp;
+}
+
+void set_ilbp_number_vector(int *number_ilbp,int result_ilbp){
+    *(number_ilbp+result_ilbp) = *(number_ilbp+result_ilbp) + 1;
+}
+
+
 void avanc_string(char *v){
     char *ptr = v;
     char *temp = malloc(9*sizeof(char));
