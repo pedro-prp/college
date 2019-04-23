6ddd562e2ccc51555a529ec6efe0425c9e11544f
pedro-prp <peddroprp@gmail.com>
2019-04-22 21:31:56 -0300

    Bugs correction

--- Projeto2/src/main.c
+++ Projeto2/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void avanc_string(char *v);
 
@@ -20,64 +21,72 @@ int main(){
     }
 
     // pega um pixel
-    int i = 1;
-    int j = 1;
     double sum = 0;
+    for(int i = 1; i < 2; i++){
+        for(int j = 1; j < 5; j++){
+            sum = 0;
+            // int *area;
+            // area = (int*) malloc(3*3*sizeof(int));
+            int area[3][3];
+            int binario[3][3];
 
-    int area[3][3];
-    int binario[3][3];
+            int lin = 0, col = 0;
+            for(int a = i-1; a < i+2; a++){
+                col = 0;
+                for(int b = j-1; b < j+2;b++){
+                    area[lin][col] = matrix[a][b];
+                    printf("%d ",area[lin][col]);
+                    sum+= area[lin][col];
+                    col++;
+                }
+                lin++;
+                printf("\n");
+            }
+            printf("%lf\n",sum/9);
+            // faz a matriz binaria
+            for(int a = 0; a < 3;a++){
+                for(int b = 0; b < 3; b++){
+                    // if(*(area+(a*3)+b) > (sum/9))
+                    if(area[a][b] > (sum/9))
+                        binario[a][b] = 1;
+                    else
+                        binario[a][b] = 0;
 
-    for(int a = i-1; a < i+2; a++){
-        for(int b = j-1; b < j+2;b++){
-            area[a][b] = matrix[a][b];
-            sum+= area[a][b];
+                    printf("%d %lf %d\n",area[a][b],(sum/9),binario[a][b]);
+                }
+            }
+
+            // caracol
+
+            char bin_str[9];
+
+            bin_str[0] = (char)binario[0][0]+48;
+            bin_str[1] = (char)binario[0][1]+48;
+            bin_str[2] = (char)binario[0][2]+48;
+            bin_str[3] = (char)binario[1][2]+48;
+            bin_str[4] = (char)binario[2][2]+48;
+            bin_str[5] = (char)binario[2][1]+48;
+            bin_str[6] = (char)binario[2][0]+48;
+            bin_str[7] = (char)binario[1][0]+48;
+            bin_str[8] = (char)binario[1][1]+48;
+
+            int result_ilbp = (int) strtol(bin_str, NULL, 2);
+
+            for(int a = 0; a < 8;a++){
+                avanc_string(&bin_str);
+                
+                if((int) strtol(bin_str, NULL, 2) < result_ilbp)
+                    result_ilbp = (int) strtol(bin_str, NULL, 2);
+                
+            }
+
+            printf("%d\n",result_ilbp);
+
+            for(int a = 0; a < 3; a++)
+                for(int b = 0; b < 3; b++)
+                    area[a][b] = 0;
         }
     }
-
-    printf("%lf\n",sum/9);
-
-    // faz a matriz binaria
-    for(int a = 0; a < 3;a++){
-        for(int b = 0; b < 3; b++){
-            if(area[a][b] > (sum/9))
-                binario[a][b] = 1;
-            else
-                binario[a][b] = 0;
-
-            printf("%d ",binario[a][b]);
-        }
-        printf("\n");
-    }
-
-    // caracol
-
-    char bin_str[9];
-    
-    bin_str[0] = (char)binario[0][0]+48;
-    bin_str[1] = (char)binario[0][1]+48;
-    bin_str[2] = (char)binario[0][2]+48;
-    bin_str[3] = (char)binario[1][2]+48;
-    bin_str[4] = (char)binario[2][2]+48;
-    bin_str[5] = (char)binario[2][1]+48;
-    bin_str[6] = (char)binario[2][0]+48;
-    bin_str[7] = (char)binario[1][0]+48;
-    bin_str[8] = (char)binario[1][1]+48;
-
-    printf("%s 1\n",bin_str);
-
-    int result_ilbp = (int) strtol(bin_str, NULL, 2);;
-    
-    for(int a = 0; a < 8;a++){
-        avanc_string(&bin_str);
-        
-        printf("%s %d %d\n",bin_str,a+2, (int) strtol(bin_str, NULL, 2));
-        
-        if((int) strtol(bin_str, NULL, 2) < result_ilbp)
-            result_ilbp = (int) strtol(bin_str, NULL, 2);
-        
-    }
-
-    printf("%d\n",result_ilbp);
 
     
     return 0;
