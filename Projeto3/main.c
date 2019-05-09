a04c4297e54501dd7f97a28e09df38e6feacb47f
pedro-prp <peddroprp@gmail.com>
2019-05-08 22:13:19 -0300

    Add method to initialize db

--- Projeto3/main.c
+++ Projeto3/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int menu();
 
@@ -12,11 +14,11 @@ void printa_menu();
 //  Data de nascimento (string no formato dd/mm/aaaa).
 
 struct Contato {
-    char nome[100];
-    char telefone[10];
-    char endereco[100];
+    char nome[101];
+    char telefone[11];
+    char endereco[101];
     unsigned int cep;
-    char dtNascimento[10];
+    char dtNascimento[11];
     struct Contato *prox;
     struct Contato *prev;
 
@@ -40,16 +42,56 @@ contato *listaVazia(){
     
 // }
 
-void abrir_data_base(){
+contato *abrir_data_base(int *num_contatos){
     FILE *f;
     char c;
+    char trash[100];
     f = fopen("contatos.txt", "r");
 
-    while(!(feof(f))){
-        // Pega 1 contato
+    // conta linhas e contatos
+    int line = 0;
+    while(!feof(f)){
+        c = fgetc(f);
+        if(c == '\n'){
+            line++;
+        }
+    } line++;
+    *num_contatos = (line/6);
+
+    contato *contatos = (contato*) malloc((line/6)*(sizeof(contato)));
+    
+    fseek (f, 0 , SEEK_SET );
+    // Pega contatos
+    for(int i = 0; i < (line/6); i++){
+        contato *aux = (contato*) malloc(sizeof(contato));
+
+        // Pega os dados de um contato
+        fgets (aux->nome, sizeof trash, f);
+        // printf("%s",aux->nome);
         
+        fgets (aux->telefone, sizeof trash, f);
+        // printf("%s",aux->telefone);
+
+        fgets (aux->endereco, sizeof trash, f);
+        // printf("%s",aux->endereco);
+
+        fgets (trash, sizeof trash, f);
+        aux->cep = (int) strtol(trash, (char **)NULL, 10);
+        // printf("%s",trash);
+
+        fgets (aux->dtNascimento, sizeof trash, f);
+        // printf("%s",aux->dtNascimento);
+        
+        fgets (trash, sizeof trash, f);
+
+        // Adiciona na lista de contatos
+        contatos[i] = *aux;
+
+        // resets
+        free(aux);
     }
-    printf("\n");
+    fclose(f);
+    return contatos;
 }
 
 
@@ -69,7 +111,16 @@ int menu(){
             printf("Jose 2\n");
             break;
         case 3:
-            abrir_data_base();
+            printf("bugzao\n");
+            contato * contatos;
+            int num_contatos;
+            contatos = abrir_data_base(&num_contatos);
+
+            for(int i = 0; i < num_contatos; i++){
+                printf("%s",contatos[i].nome);
+            }
+            
+            free(contatos);
             break;
         default:
             break;
@@ -80,7 +131,7 @@ int menu(){
 
 
 int main(){
-    int a;
+    int a = 0;
 
     while(a != 4){
         a = menu();
