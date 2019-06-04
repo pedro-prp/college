260c1b63292a219609cb5e6da657b0792da9933b
pedro-prp <peddroprp@gmail.com>
2019-06-04 19:39:57 -0300

    set struct of binary tree

--- Projeto5/main.c
+++ Projeto5/main.c
@@ -1,9 +1,25 @@
 // Projeto 5
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ArvoreBinaria{
+    int info;
+    struct ArvoreBinaria *filhoDir;
+    struct ArvoreBinaria *filhoEsq;
+} arvore;
+
+arvore *arvoreVazia();
 
 int main(){
 
-    printf("Initial\n");
+    arvore *a = arvoreVazia();
+
+    printf("a\n");
 
     return 0;
+}
+
+arvore *arvoreVazia(){
+    return NULL;
 }
