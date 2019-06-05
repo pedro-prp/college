6f6a53f2cdfc8aa1236c4c6e1274c3fc9e94eab4
pedro-prp <peddroprp@gmail.com>
2019-06-04 22:45:55 -0300

    Implement stop point to numbers vector

--- Projeto5/main.c
+++ Projeto5/main.c
@@ -16,7 +16,9 @@ arvore *LoadTreeFromFile(char *);
 
 // funções auxiliares para funcionamento da biblioteca
 arvore *arvoreVazia();
-int *leituraDeArquivo(FILE *);
+arvore *alocaNo(int info);
+double *leituraDeArquivo(FILE *);
+double checaDecimal(double);
 
 
 int main(){
@@ -33,9 +35,19 @@ arvore *arvoreVazia(){
     return NULL;
 }
 
+arvore *alocaNo(int info){
+    arvore *arv = (arvore *) malloc(sizeof(arvore));
+    
+    arv->info = info;
+    arv->filhoDir = NULL;
+    arv->filhoEsq = NULL;
+
+    return arv;
+}
+
 arvore *LoadTreeFromFile(char *path){
     arvore *arv = arvoreVazia();
-    arv = (arvore *) malloc(sizeof(arvore));    
+    double *vector;    
     
     FILE *f;  
     f = fopen(path,"r");
@@ -43,34 +55,48 @@ arvore *LoadTreeFromFile(char *path){
         printf("Erro de leitura\n");
         exit(-1);
     }
+    vector = leituraDeArquivo(f);
 
-    int *a = leituraDeArquivo(f);
+    // Raiz
+    arv = alocaNo(vector[0]);
 
+    printf("Raiz: %d\n",arv->info);
 
+    for(int i = 1; checaDecimal(vector[i]) == 0.0; i++){
+        printf("%.0lf\n",vector[i]);
+    }
 
     fclose(f);
     return arv;
 }
 
-int *leituraDeArquivo(FILE *f){
-    int cont=0,aux;
-    char jaja;
-    int *vector;
+double *leituraDeArquivo(FILE *f){
+    int cont=0;
+    double *vector;
     
     // conta quant de numeros
     while(!feof(f)){
         fscanf(f,"%*d%*c");
         cont++;
     }
-    vector = (int *) calloc(cont,sizeof(int));
+    vector = (double *) calloc((cont+1),sizeof(double));
 
     // retorna File f pro começo
     fseek( f, 0, SEEK_SET );
 
     // armazena em vector os elementos
     for(int i = 0; !feof(f); i++){
-        fscanf(f,"%d%*c",&vector[i]);
+        fscanf(f,"%lf%*c",&vector[i]);
     }
+    
+    // criterio de parada
+    vector[cont] = 1.5;
 
     return vector;
+}
+
+double checaDecimal(double d){
+    double result = d - (int)d;
+    
+    return result;
 }
