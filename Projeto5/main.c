51c3625230d2f0b504b52c57897f79c37ab0d1a1
pedro-prp <peddroprp@gmail.com>
2019-06-04 22:21:08 -0300

    Implement function to read number list of a file

--- Projeto5/main.c
+++ Projeto5/main.c
@@ -9,17 +9,68 @@ typedef struct ArvoreBinaria{
     struct ArvoreBinaria *filhoEsq;
 } arvore;
 
+// biblioteca
+arvore *LoadTreeFromFile(char *);
+
+
+
+// funções auxiliares para funcionamento da biblioteca
 arvore *arvoreVazia();
+int *leituraDeArquivo(FILE *);
+
 
 int main(){
 
-    arvore *a = arvoreVazia();
+    char in[100];
+    scanf("%s",in);
 
-    printf("a\n");
+    LoadTreeFromFile(in);
 
     return 0;
 }
 
 arvore *arvoreVazia(){
     return NULL;
+}
+
+arvore *LoadTreeFromFile(char *path){
+    arvore *arv = arvoreVazia();
+    arv = (arvore *) malloc(sizeof(arvore));    
+    
+    FILE *f;  
+    f = fopen(path,"r");
+    if(f == NULL){
+        printf("Erro de leitura\n");
+        exit(-1);
+    }
+
+    int *a = leituraDeArquivo(f);
+
+
+
+    fclose(f);
+    return arv;
+}
+
+int *leituraDeArquivo(FILE *f){
+    int cont=0,aux;
+    char jaja;
+    int *vector;
+    
+    // conta quant de numeros
+    while(!feof(f)){
+        fscanf(f,"%*d%*c");
+        cont++;
+    }
+    vector = (int *) calloc(cont,sizeof(int));
+
+    // retorna File f pro começo
+    fseek( f, 0, SEEK_SET );
+
+    // armazena em vector os elementos
+    for(int i = 0; !feof(f); i++){
+        fscanf(f,"%d%*c",&vector[i]);
+    }
+
+    return vector;
 }
