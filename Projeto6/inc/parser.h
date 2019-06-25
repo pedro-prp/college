47e8ef3f0da5d38828ef3d4b669f3f4f657d099a
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2019-06-25 11:05:05 -0300

    complecao do parser e adicao dos arquivos txt gerados utilizando o trabalho 2

--- Projeto6/inc/parser.h
+++ Projeto6/inc/parser.h
@@ -1,5 +1,63 @@
+
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include "projeto2.h"
 
-#endif 
+//parser sorteia arquivos para treino e teste
+void parser()
+{
+    printf("\t Arquivos ja sorteados e vetorizados!\n\t Separando arquivos para teste e treino..\n\n");
+    FILE *testeAG;
+    FILE *treinoAG;
+    FILE *asphalt;
+    FILE *grass;
+    asphalt = fopen("asphalt.txt", "r");
+    grass = fopen("grass.txt", "r");
+    testeAG = fopen("teste.txt", "w+");
+    treinoAG = fopen("treino.txt", "w+");
+    int z = 0;
+    char tranf;
+    while (z < 25)
+    {
+        tranf = fgetc(asphalt);
+        fprintf(testeAG, "%c", tranf);
+        if (tranf == '\n')
+            z++;
+    }
+    z = 0;
+    while (z < 25)
+    {
+        tranf = fgetc(grass);
+        fprintf(testeAG, "%c", tranf);
+        if (tranf == '\n')
+            z++;
+    }
+    z = 25;
+    while (z < 50)
+    {
+        tranf = fgetc(asphalt);
+        fprintf(treinoAG, "%c", tranf);
+        if (tranf == '\n')
+            z++;
+    }
+    z = 25;
+    while (z < 50)
+    {
+        tranf = fgetc(grass);
+        fprintf(treinoAG, "%c", tranf);
+        if (tranf == '\n')
+            z++;
+    }
+
+    fclose(testeAG);
+    fclose(treinoAG);
+    fclose(asphalt);
+    fclose(grass);
+}
+#endif
